9904c22a2db90526db125212192d817c770ef1ca
andreyk1n <betas23wr@gmail.com>
2025-11-14 21:39:20 +0200

    upd starter structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-// import logo from './logo.svg';
-// import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./template-parts/partials/header/Header";
+import Footer from "./template-parts/partials/footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      my app
-    </div>
+    <Router>
+      <Header />
+
+
+      <Footer />
+    </Router>
   );
 }
 
