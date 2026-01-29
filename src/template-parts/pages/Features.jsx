ce9e86299400c4f2f36b269a250a6a1217f91d64
andreyk1n <betas23wr@gmail.com>
2026-01-29 19:17:11 +0200

    upd pages/features

--- src/template-parts/pages/Features.jsx
+++ src/template-parts/pages/Features.jsx
@@ -1,7 +1,27 @@
+import Hero from "../components/hero/Hero.jsx";
+import heroImage from "../../images/hero/hero--features.svg";
+
+
 const Features = () => {
-    return ( 
-        <>Features</>
-     );
-}
- 
+  return (
+    <>
+      <Hero
+        title="All the features you need"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        image={{
+          src: heroImage,
+          alt: "Hero illustration",
+        }}
+        buttons={[
+          {
+            label: "View pricing",
+            className: "button--yellow",
+            onClick: () => console.log("Work"),
+          }
+        ]}
+      />
+    </>
+  );
+};
+
 export default Features;
