0cd0c6105e1df5f99f7c2fb9154c784e353f5986
andreyk1n <betas23wr@gmail.com>
2026-01-29 19:12:10 +0200

    upd pricing

--- src/template-parts/pages/Pricing.jsx
+++ src/template-parts/pages/Pricing.jsx
@@ -1,15 +1,46 @@
 import Price from "../components/price/Price.jsx";
 import Accordion from "../components/accordion/Accordion.jsx";
 
-
 const Pricing = () => {
-    return ( 
-        <>
-        <Price />
+  const accordionData = [
+    {
+      title: "How much time does it take?",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      title: "What is your class naming convention?",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      title: "How do we communicate?",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      title: "I have a bigger project. Can you handle it?",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      title: "What is your class naming convention?",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+  ];
+  return (
+    <>
+      <Price />
 
-        <Accordion />
-        </>
-     );
-}
- 
+      <Accordion
+        title="Frequently asked questions"
+        description="Contact us for more info"
+        items={accordionData}
+        defaultActiveIndex={0}
+      />
+    </>
+  );
+};
+
 export default Pricing;
