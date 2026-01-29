24bb67dccf2738e0e6be14cc7b944c765ef8893b
andreyk1n <betas23wr@gmail.com>
2026-01-29 19:22:01 +0200

    upd pages/features

--- src/template-parts/pages/Features.jsx
+++ src/template-parts/pages/Features.jsx
@@ -1,6 +1,6 @@
 import Hero from "../components/hero/Hero.jsx";
+import Cards from "../components/cards/Cards.jsx";
 import heroImage from "../../images/hero/hero--features.svg";
-
 
 const Features = () => {
   return (
@@ -17,7 +17,30 @@ const Features = () => {
             label: "View pricing",
             className: "button--yellow",
             onClick: () => console.log("Work"),
-          }
+          },
+        ]}
+      />
+
+      <Cards
+        label=""
+        className="cards--blue"
+        title="The benefits of working with our team"
+        items={[
+          {
+            icon: 6,
+            title: "Customize with ease",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim.",
+          },
+          {
+            icon: 3,
+            title: "Perfectly Responsive",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim.",
+          },
+          {
+            icon: 1,
+            title: "Friendly Support",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim.",
+          },
         ]}
       />
     </>
