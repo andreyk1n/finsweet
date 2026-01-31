e13e77a7dbd0bc25b0dddd815c9c2a61947c2035
andreyk1n <betas23wr@gmail.com>
2026-01-31 09:19:49 +0200

    update pages/Blog,jsx and pages/Post.jsx

--- src/template-parts/pages/Blog.jsx
+++ src/template-parts/pages/Blog.jsx
@@ -1,12 +1,18 @@
+import useDocumentMeta from "../components/useDocumentMeta/useDocumentMeta.js";
 import { posts } from "../../data/posts";
 import BlogSection from "../components/blog/BlogSection";
 
 const Blog = () => {
-    return ( 
-        <>
-        <BlogSection posts={posts} />
-        </>
-     );
-}
- 
+  useDocumentMeta({
+    title: "Finsweet | Our blog",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  });
+  return (
+    <>
+      <BlogSection posts={posts} />
+    </>
+  );
+};
+
 export default Blog;
--- src/template-parts/pages/Post.jsx
+++ src/template-parts/pages/Post.jsx
@@ -1,12 +1,25 @@
+import useDocumentMeta from "../components/useDocumentMeta/useDocumentMeta.js";
 import Single from "../components/single/Single.jsx";
+import { useParams } from "react-router-dom";
+import { posts } from "../../data/posts";
 
 
 const Post = () => {
-    return ( 
-        <>
-        <Single />
-        </>
-     );
-}
- 
+  const { id } = useParams();
+
+  const post = posts.find(p => p.id === Number(id));
+
+  useDocumentMeta({
+    title: post ? `Finsweet | ${post.title}` : "Finsweet | Post",
+    description: post?.summary || "Blog post page",
+  });
+
+  if (!post) return null;
+  return (
+    <>
+      <Single />
+    </>
+  );
+};
+
 export default Post;
