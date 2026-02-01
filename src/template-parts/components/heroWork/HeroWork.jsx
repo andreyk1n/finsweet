8642aface982f70c3068c28171891b5af53b9b44
andreyk1n <betas23wr@gmail.com>
2026-02-01 11:08:28 +0200

    feat: add HeroWork component and integrate it into Works page

---
+++ src/template-parts/components/heroWork/HeroWork.jsx
@@ -0,0 +1,15 @@
+import "./hero.scss";
+
+const HeroWork = () => {
+    return ( 
+        <div className="hero">
+            <div className="hero__container">
+                <span className="hero__label">What we created</span>
+                <h1 className="hero__title">Our Work Portfolio</h1>
+                <p className="hero__description">We help teams create great digital products by providing them with tools and technology to make the design-to-code process universally accessible.</p>
+            </div>
+        </div>
+     );
+}
+ 
+export default HeroWork;
--- src/template-parts/pages/Works.jsx
+++ src/template-parts/pages/Works.jsx
@@ -1,14 +1,17 @@
 import useDocumentMeta from "../components/useDocumentMeta/useDocumentMeta.js";
 import Works from "../components/works/Works.jsx";
+import HeroWork from "../components/heroWork/HeroWork.jsx";
 
 const Contacts = () => {
   useDocumentMeta({
     title: "Finsweet | Our works",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   });
   return (
     <>
-        <Works />
+      <HeroWork />
+      <Works />
     </>
   );
 };
